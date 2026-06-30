715a3c1ff0e5276e37af719afbb8cacd57b8e1d9
Ruben-coder66 <ruben.rivadeneira@trade.ec>
2026-06-30 10:55:48 -0500

    Actualizar Base

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-const TARGET = 'https://cls59-dal.i6.inconcert.cloud/inconcert/api';
+const TARGET = 'https://gruponuveto-aws.i6.inconcert.cloud/inconcert/api';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Solo POST
